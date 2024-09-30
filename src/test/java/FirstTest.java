b4837c8321ef3e52f42e0b2a1934907bb824aada
Mahmoud17 <mahmoud30ayoub@gmail.com>
2024-09-30 19:52:04 +0300

    section 3 done

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -7,10 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import java.time.Duration;
 import java.util.List;
 
 public class FirstTest {
